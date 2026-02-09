6f5bf86fa67a5d30f2860b53668bf04f64bd0bb9
jryurgil <jryurgil@gmail.com>
2026-02-09 18:48:05 -0500

    added demo pneumatics code for test on old robot

---
+++ src/main/java/frc/robot/subsystems/DemoPneumatics.java
@@ -0,0 +1,66 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.ProtoConstants.*;
+
+public class DemoPneumatics extends SubsystemBase {
+  /* private final SparkMax feederRoller;
+  private final SparkMax intakeLauncherRoller; */
+//private final TalonSRX feederRoller; //Rollers on back of prototype launcher
+//private final TalonSRX intakeRoller; //Orange star at center of launcher
+private final DoubleSolenoid claw;
+
+
+  /** Creates a new CANBallSubsystem. */
+  public DemoPneumatics() {
+    // create brushed motors for each of the motors on the launcher mechanism
+    /* intakeLauncherRoller = new SparkMax(INTAKE_LAUNCHER_MOTOR_ID, MotorType.kBrushed);
+    feederRoller = new SparkMax(FEEDER_MOTOR_ID, MotorType.kBrushed); */
+
+ // intakeRoller = new TalonSRX(PROTO_INTAKE_MOTOR_ID);
+ // feederRoller = new TalonSRX(PROTO_FEEDER_MOTOR_ID);
+  //launcherRoller = new SparkFlex(PROTO_LAUNCHER_MOTOR_ID, MotorType.kBrushless);
+  claw = new DoubleSolenoid(PneumaticsModuleType.REVPH, 8, 9);
+
+
+    
+    // put default values for various fuel operations onto the dashboard
+    // all commands using this subsystem pull values from the dashbaord to allow
+    // you to tune the values easily, and then replace the values in Constants.java
+    // with your new values. For more information, see the Software Guide.
+    SmartDashboard.getBoolean("extend", false);
+    
+  }
+
+public void extend(boolean extendBool){
+  if(extendBool){
+    claw.set(Value.kForward);
+  }else{
+    claw.set(Value.kReverse);
+  }
+  
+
+}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
